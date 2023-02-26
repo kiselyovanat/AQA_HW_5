6475e9ce77f11fd696cbe5f9065f481accd93bf8
Kiseleva Natalia <kiselyov-natalya@mail.ru>
2023-02-26 21:52:29 +0300

    add Drag and Drop test

---
+++ src/test/java/DragAndDropTest.java
@@ -0,0 +1,24 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+public class DragAndDropTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
+        Configuration.holdBrowserOpen = true;
+    }
+    @Test
+    void dragAndDropTest() {
+        open("/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-a").$("header").shouldHave(text("B"));
+        $("#column-b").$("header").shouldHave(text("A"));
+
+    }
+}
